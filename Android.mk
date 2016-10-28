CyanogenMod/android_packages_apps_BluetoothExt

Add makefile guard, this needs proprietary parts now.
Change-Id: I9f0579ef5f1590612bc14f401f1f682ddd5fc5e9

Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_USES_AOSP),true)
+ifeq ($(BOARD_HAVE_QCOM_BLUETOOTH),true)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)

QuantumDroid/platform_vendor_qcom-opensource_bluetooth

Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -30,4 +32,7 @@ include $(BUILD_PACKAGE)
 
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
 #endif
+
+endif